0f601d5781ec437b2abe47af575a96702055b93e
Joseph Derkingit config --global user.email jd039218@ohio.edugit config --global user.name Joseph <josephderkin@Josephs-MacBook-Pro.local>
2019-05-16 14:06:07 -0400

    Initial Commit of git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Joseph Derkin
+ *        Date: Thursday, May 16 2019
+ * Description: Homework 1 CS 5900
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!" << endl;
+    return 0;
+    
+}// main
